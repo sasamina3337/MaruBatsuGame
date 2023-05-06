788d2843451fb668de3fe3c4e48375daf2c9623e
DESKTOP-565GR93\sasam <iwaoku.ryoki@gmail.com>
2023-05-07 00:29:04 +0900

    自分のターンを選んで、ボタンを押せるところまで作成した。

--- 3目並べ/Form1.Designer.cs
+++ 3目並べ/Form1.Designer.cs
@@ -45,7 +45,7 @@
             // start
             // 
             this.start.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.start.Location = new System.Drawing.Point(164, 431);
+            this.start.Location = new System.Drawing.Point(184, 431);
             this.start.Name = "start";
             this.start.Size = new System.Drawing.Size(73, 28);
             this.start.TabIndex = 0;
@@ -55,81 +55,99 @@
             // 
             // button0
             // 
-            this.button0.Location = new System.Drawing.Point(46, 105);
+            this.button0.Enabled = false;
+            this.button0.Location = new System.Drawing.Point(64, 105);
             this.button0.Name = "button0";
             this.button0.Size = new System.Drawing.Size(100, 100);
             this.button0.TabIndex = 1;
             this.button0.UseVisualStyleBackColor = true;
+            this.button0.Click += new System.EventHandler(this.buttonALl_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(149, 105);
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(167, 105);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 100);
             this.button1.TabIndex = 2;
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.buttonALl_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(252, 105);
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(270, 105);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(100, 100);
             this.button2.TabIndex = 3;
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.buttonALl_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(46, 211);
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(64, 211);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(100, 100);
             this.button3.TabIndex = 4;
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.buttonALl_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(149, 211);
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(167, 211);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(100, 100);
             this.button4.TabIndex = 5;
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.buttonALl_Click);
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(252, 211);
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(270, 211);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(100, 100);
             this.button5.TabIndex = 6;
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.buttonALl_Click);
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(46, 317);
+            this.button6.Enabled = false;
+            this.button6.Location = new System.Drawing.Point(64, 317);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(100, 100);
             this.button6.TabIndex = 7;
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.buttonALl_Click);
             // 
             // button8
             // 
-            this.button8.Location = new System.Drawing.Point(252, 317);
+            this.button8.Enabled = false;
+            this.button8.Location = new System.Drawing.Point(270, 317);
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(100, 100);
             this.button8.TabIndex = 8;
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.buttonALl_Click);
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(149, 317);
+            this.button7.Enabled = false;
+            this.button7.Location = new System.Drawing.Point(167, 317);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(100, 100);
             this.button7.TabIndex = 9;
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.buttonALl_Click);
             // 
             // title
             // 
             this.title.AutoSize = true;
             this.title.Font = new System.Drawing.Font("ＭＳ ゴシック", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.title.Location = new System.Drawing.Point(40, 21);
+            this.title.Location = new System.Drawing.Point(58, 21);
             this.title.Name = "title";
             this.title.Size = new System.Drawing.Size(319, 33);
             this.title.TabIndex = 10;
@@ -141,7 +159,7 @@
             this.label.AutoSize = true;
             this.label.Font = new System.Drawing.Font("ＭＳ ゴシック", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.label.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label.Location = new System.Drawing.Point(99, 69);
+            this.label.Location = new System.Drawing.Point(117, 69);
             this.label.Name = "label";
             this.label.Size = new System.Drawing.Size(199, 15);
             this.label.TabIndex = 11;
@@ -152,7 +170,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(395, 471);
+            this.ClientSize = new System.Drawing.Size(439, 471);
             this.Controls.Add(this.label);
             this.Controls.Add(this.title);
             this.Controls.Add(this.button7);
--- 3目並べ/Form1.cs
+++ 3目並べ/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.Contracts;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,16 @@ namespace _3目並べ
 {
     public partial class Form1 : Form
     {
+        public string[] mark = new string[] { "", "○", "×" };
+
+        public Boolean PrimaryTurn;
+
+        public int gameCount, Turn;
+
+        public int[] gameBoard = new int[9];
+
+        public Boolean YourTurn;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +30,64 @@ namespace _3目並べ
 
         private void start_Click(object sender, EventArgs e)
         {
+            BoardEnable(true);
+            gameCount = 0;
+            ButtonMemory();
+            Random random = new Random();
+            YourTurn = (random.Next(2) == 0);
 
+        }
+        public void BoardEnable(Boolean l)
+        {
+            Control[] c;
+            Button btn;
+            for (int i = 0; i < gameBoard.Length; i++)
+            {
+                c = this.Controls.Find("button" + i.ToString(), true);
+                btn = (Button)c[0];
+                btn.Enabled = l;
+            }
+        }
+
+        private void buttonALl_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            ButtonMemory();
+            gameCount++;
+            if (YourTurn)
+            {
+                btn.Text = mark[1];
+            }
+            else
+            {
+                btn.Text = mark[2];
+            }
+        }
+
+        public void ButtonMemory()
+        {
+            Control[] d;
+            Button btn;
+            for (int i = 0; i < gameBoard.Length && i < 9; i++)
+            {
+                d = this.Controls.Find("button" + i.ToString(), true);
+                btn = (Button)d[0];
+
+                if (btn.Text == mark[0])
+                {
+                    gameBoard[i] = 0;
+                }
+                else if (btn.Text == mark[1])
+                {
+                    gameBoard[i] = 1;
+                    btn.Enabled = false;
+                }
+                else
+                {
+                    gameBoard[i] = 2;
+                    btn.Enabled = false;
+                }
+            }
         }
     }
 }
